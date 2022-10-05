9efaeb26f7ace5bc6b67566eaac2afed144d3701
zlatan-dp <zlatandnepr@gmail.com>
2022-10-05 16:56:38 +0300

    end test 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-import React from 'react';
+import ReactDOM from "react-dom/client";
+
+const favoriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" },
+  { id: "id-4", name: "Redux in depth" },
+];
+
+const BookList = ({ books }) => {
+  return (
+    <ul>
+      {books.map((book) => (
+        <li key={book.id}>{book.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <BookList books={favoriteBooks} />
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+// ===============================
+/* import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -14,4 +44,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); */
